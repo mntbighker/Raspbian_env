c97d1bb5beff446763c5dabd22a21101ea8aeca1
Mark Moorcroft <mntbighker@gmail.com>
2023-02-26 11:18:15 -0800

    Update run1.sh

--- run1.sh
+++ run1.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # https://github.com/emilyst/home
 
-cd ~/
-
+cd $HOME
 curl -O https://distfiles.macports.org/MacPorts/MacPorts-2.8.1.tar.bz2
 tar xf MacPorts-2.8.1.tar.bz2
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 cd MacPorts-2.8.1
 ./configure --prefix=$HOME/local --with-install-user=$USER --with-install-group=staff --without-startupitems
 make && make install
-
-
