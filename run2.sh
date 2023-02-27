b86b466462a094981311d0b0d0f019def310e939
Mark Moorcroft <mntbighker@gmail.com>
2023-02-26 17:11:18 -0800

    Update run2.sh

--- run2.sh
+++ run2.sh
@@ -1,11 +1,8 @@
 #!/bin/zsh
 
 export PATH=$HOME/local/bin:$PATH
-
 port selfupdate && port upgrade outdated
-
 $HOME/local/bin/port -N install autoconf automake bison bison-runtime boost176 brotli bzip2 cargo cmake cmake-bootstrap curl curl-ca-bundle docbook-xml docbook-xsl-nons expat gettext gettext-runtime gettext-tools-libs gmp gperf icu libarchive libb2 libcxx libedit libevent libffi libgit2 libiconv libidn2 libpsl libssh2 libtermkey libtextstyle libtool libunistring libutf8proc libuv libvterm libxml2 lua51 lua51-lpeg lua51-luarocks lua51-mpack luajit luarocks_select luv-luajit lz4 lzma lzo2 m4 msgpack msgpack-c msgpack-cpp ncurses neovim nghttp2 ninja nodejs19 npm9 openssl openssl3 p7zip pcre2 pkgconfig python3_select python311 python_select re2c readline rlwrap rust sqlite3 tmux tmux-pasteboard tree-sitter-cli unibilium wget xmlcatmgr xz zlib zstd
-
 port -f activate python311
 port select --set python python311
 port select --set python3 python311
