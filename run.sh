a4c13c5fa65473eda28284df082bf50060f8452b
Mark Moorcroft <mntbighker@gmail.com>
2023-03-01 20:10:23 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,3 +11,4 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 mv .zshrc.pre-oh-my-zsh .zshrc
+mv bigpath.zsh-theme .oh-my-zsh/themes/
