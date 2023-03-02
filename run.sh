f3228c07f47f699667e74fc29e54e48ba289a206
Mark Moorcroft <mntbighker@gmail.com>
2023-03-01 16:39:04 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,6 +9,5 @@ sh ./install.sh
 rm install.sh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
 mv .zshrc.pre-oh-my-zsh .zshrc
