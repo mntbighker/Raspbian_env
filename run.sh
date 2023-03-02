17f31fa5d057138d83d7ad3e1cbf4f59fe87f813
Mark Moorcroft <mntbighker@gmail.com>
2023-03-02 11:50:52 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,11 +5,18 @@ sudo dnf -y install neovim python39 luarocks zsh
 tmux set-option -g default-shell /usr/bin/zsh
 
 cd ~/
-# sh ./install.sh
-rm install.sh
+rm -rf ~/Linux_env/.git
+mv ~/Linux_env/.config ~/
+mv ~/Linux_env/.tmux.conf ~/
+mv ~/Linux_env/.zshrc ~/
+mv ~/Linux_env/install.sh ~/
+sh ~/install.sh
+rm ~/install.sh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
 mv .zshrc.pre-oh-my-zsh .zshrc
 # mv bigpath.zsh-theme .oh-my-zsh/themes/
+
+rm -rf Linux_env
