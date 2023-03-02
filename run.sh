1c71888a7388ba23232ea408b86370abd96dc0e1
Mark Moorcroft <mntbighker@gmail.com>
2023-03-02 12:15:39 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -12,8 +12,8 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
-rm .zshrc.pre-oh-my-zsh
+rm ~/.zshrc.pre-oh-my-zsh
 mv ~/Linux_env/.zshrc ~/
 # mv bigpath.zsh-theme .oh-my-zsh/themes/
 
-rm -rf Linux_env
+rm -rf ~/Linux_env
