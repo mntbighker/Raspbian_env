3f642f703acd4f1c168ad9e74fe89c6b81a5f3b0
Mark Moorcroft <mntbighker@gmail.com>
2023-03-03 18:17:09 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
 
-if [ $USER = "root" ]; then
-  sudo dnf -y install neovim python39 luarocks zsh npm
+if ! [ -f $HOME/Linux_env/.tmux.conf ]; then
+  echo "Please git clone into $HOME before running run.sh\n"
   exit
 fi
 
-cd ~/
+if ! [ -f /usr/bin/nvim ]; then
+  sudo dnf -y install neovim python39 luarocks zsh npm tmux
+  exit
+fi
+
+cd $HOME
 rm -rf .config
 mv Linux_env/.config .
 mv Linux_env/.tmux.conf .
 mv Linux_env/install.sh .
-echo "### type exit after the ohmyzsh script finishes, to complete setup ###"
-sh ./install.sh
-rm ./install.sh
+echo "### Type exit after the oh-my-zsh install script finishes, to complete setup ###\n"
+sh ./install.sh && rm ./install.sh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
-rm .zshrc.pre-oh-my-zsh
 rm ./.zshrc
 mv Linux_env/.zshrc .
-# mv bigpath.zsh-theme .oh-my-zsh/themes/
 
 rm -rf Linux_env
