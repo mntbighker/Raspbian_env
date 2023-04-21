e5a5e3c74ea2acce49391afdd040c9580f2a5c5c
Mark Moorcroft <mntbighker@gmail.com>
2023-04-21 11:58:03 -0700

    Update run8.sh

--- run8.sh
+++ run8.sh
@@ -6,16 +6,16 @@ if ! [ -f $HOME/Linux_env/.tmux.conf ]; then
 fi
 
 if ! [ -f /usr/bin/nvim ]; then
-  sudo dnf -y module reset ruby
-  sudo dnf -y module reset nodejs
-  sudo dnf -y module enable ruby:2.6
-  sudo dnf -y module enable nodejs:18
-  sudo dnf -y remove ruby*
+  sudo dnf -y module reset ruby # colorls
+  sudo dnf -y module reset nodejs # colorls
+  sudo dnf -y module enable ruby:2.6 # colorls
+  sudo dnf -y module enable nodejs:18 #colorls
+  sudo dnf -y remove ruby* # colorls
   # sudo dnf -y config-manager --set-enabled ol8_appstream # Oracle
   # sudo dnf -y install epel-release-el8 # Oracle
   sudo dnf -y install epel-release # RHEL
-  sudo dnf -y group install "Development Tools"
-  sudo dnf -y install neovim python39 ruby ruby-devel rubygems luarocks zsh npm nodejs cargo cmake tmux wget
+  sudo dnf -y install python39 ruby ruby-devel rubygems nodejs cmake # for colorls
+  sudo dnf -y install gcc gcc-c++ neovim luarocks zsh npm tmux wget # for neovim
 fi
 
 cd $HOME
@@ -24,7 +24,6 @@ mv Linux_env/.config .
 mv Linux_env/.tmux.conf .
 mv Linux_env/install.sh .
 gem install colorls
-cargo install tree-sitter-cli
 echo -e "### Type exit after the oh-my-zsh install script finishes, to complete setup ###\n"
 sh ./install.sh && rm ./install.sh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
