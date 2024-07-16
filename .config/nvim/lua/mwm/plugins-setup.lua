b0fe3485f357424d810ceeba65ae0e4b01830b84
Mark Moorcroft <mntbighker@gmail.com>
2024-07-15 18:45:49 -0700

    Update plugins-setup.lua

--- .config/nvim/lua/mwm/plugins-setup.lua
+++ .config/nvim/lua/mwm/plugins-setup.lua
@@ -107,6 +107,14 @@ return packer.startup(function(use)
   -- git integration
   use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
 
+    -- LazyGit
+  use({"kdheepak/lazygit.nvim",
+    -- optional for floating window border decoration
+    requires = {
+        "nvim-lua/plenary.nvim",
+    },
+  })
+
   -- fzf
   use { "ibhagwan/fzf-lua",
   -- optional for icon support
